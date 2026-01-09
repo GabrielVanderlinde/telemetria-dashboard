3faf0f229779d49617f4652e801d48cd5b17a8c2
unknown <you@example.com>
2026-01-09 10:29:12 -0300

    feat: adiciona e implementa lógica js relacionada ao mapa

--- FRONT/script.js
+++ FRONT/script.js
@@ -36,3 +36,34 @@ function mostrarCard(dados) {
     // Mostra o card
     document.getElementById('card-clima').style.display = 'flex';
 }
+
+// --- SISTEMA DE ABAS (TABS) ---
+
+// 1. Pegando os elementos do HTML pelo ID
+const btnGeral = document.getElementById('btn-geral');
+const btnMapa = document.getElementById('btn-mapa');
+
+const secaoGeral = document.getElementById('secao-geral');
+const secaoMapa = document.getElementById('secao-mapa');
+
+// 2. Função para abrir o Mapa de Risco
+btnMapa.addEventListener('click', function () {
+    // Lógica: Esconde um, mostra o outro
+    secaoGeral.style.display = 'none'; // Some a Visão Geral
+    secaoMapa.style.display = 'block'; // Aparece o Mapa
+
+    // (Visual) Troca a classe 'ativo' no menu para o botão ficar vermelho
+    btnGeral.classList.remove('ativo');
+    btnMapa.classList.add('ativo');
+});
+
+// 3. Função para voltar para a Visão Geral
+btnGeral.addEventListener('click', function () {
+    // Lógica: Esconde um, mostra o outro
+    secaoMapa.style.display = 'none'; // Some o Mapa
+    secaoGeral.style.display = 'block'; // Aparece a Visão Geral
+
+    // (Visual) Troca a classe 'ativo' de volta
+    btnMapa.classList.remove('ativo');
+    btnGeral.classList.add('ativo');
+});
